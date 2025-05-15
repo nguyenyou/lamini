6994266fa70a41413e0d2f21ba848080a1c025a0
nguyenyou <tunt081295@gmail.com>
2025-05-16 01:34:42 +0700

    🚢 ship it

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 
+const IS_PROD = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [],
-  base: "/lamini/",
+  base: IS_PROD ? "/lamini/" : "/",
 });
